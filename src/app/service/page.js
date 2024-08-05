a54b3fa8781d04f58c894194d80e05ca04cfcc3f
housebanao-main <tech@housebanao.com>
2024-08-05 11:29:17 +0530

    Update page.js

--- src/app/service/page.js
+++ src/app/service/page.js
@@ -12,7 +12,7 @@ const service = () => {
             <House />
             <OfferTwo />
             <CraftSection />
-            <Journey />
+            
             <Faqs />
          
         </div>
