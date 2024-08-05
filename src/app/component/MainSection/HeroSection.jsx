6323d92a34d10a233496fa35ba2e58c64779354c
housebanao-main <tech@housebanao.com>
2024-08-05 12:13:21 +0530

    Update HeroSection.jsx

--- src/app/component/MainSection/HeroSection.jsx
+++ src/app/component/MainSection/HeroSection.jsx
@@ -10,7 +10,7 @@ export const HeroSection = ({ altText }) => {
   return (
     <div className="relative w-full">
       <img src="/assets/images/bannerimage.jpeg" alt={altText} className='w-full h-[600px] lg:h-[700px] object-cover object-bottom'/>
-      <a href="https://api.whatsapp.com/send?phone=9810432124" target="_blank" rel="noopener noreferrer" className="cursor-pointer inline z-[20] fixed right-0 overflow-hidden bg-white px-[10px] py-[7px] rounded-l-[6px] shadow-xl flex items-center w-[55px] h-[55px] top-[118px]">
+      <a href="https://api.whatsapp.com/send?phone=+919810432124" target="_blank" rel="noopener noreferrer" className="cursor-pointer inline z-[20] fixed right-0 overflow-hidden bg-white px-[10px] py-[7px] rounded-l-[6px] shadow-xl flex items-center w-[55px] h-[55px] top-[118px]">
         <Image src={whatsappicon} alt='whatsapp' className='w-[34px]' />
       </a>
       <a href="tel:+919810432124" className="cursor-pointer inline z-[20] fixed right-0 overflow-hidden bg-white px-[10px] py-[7px] rounded-l-[6px] shadow-xl flex items-center w-[55px] h-[55px] top-[180px]">
